f3f62be5cdc16def2d7ebee539478a3528956e71
Ryan Orrico <ryanorrico@Ryans-MacBook-Pro.local>
2012-11-06 20:01:07 -0600

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,30 @@
 class UsersController < ApplicationController
   def new
   	@title = "Sign up"
+  	@user = User.new
   end
+
+  def show 
+  	@user = User.find(params[:id])
+  	@title = @user.name
+  end
+
+  def index
+  	@title = "All users"
+  	@users = User.all
+  end
+
+
+  def create
+ 		@title = "fuck"
+  	@user = User.new(params[:user])
+  	if @user.save
+  		flash[:success] = "Welcome to the Sample App!"
+  		redirect_to @user
+  	else
+  		render 'new'
+  	end
+
+  end
+
 end
---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do 
+	factory :user do 
+		name				"Ryan Orrico"
+		email				"ryanrules@gmail.com"
+		password 		"foobar"
+		password_confirmation		"foobar"
+	end
+
+end
